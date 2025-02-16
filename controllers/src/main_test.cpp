7b1fcf0b40e63da4f832447c7160aca1de1c6134
Arcod7 <antoine.esman@epitech.eu>
2025-02-16 15:44:20 +0100

    feat(servo_test): dynamically change the max_angle with a potentiometer

--- controllers/src/main_test.cpp
+++ controllers/src/main_test.cpp
@@ -1,7 +1,7 @@
 // #include <Arduino.h>
 // #include <Servo.h>
 
-// #define MAX_ANGLE 180
+// int max_angle = 180;
 // // MAX_ANGLE is 180 for small servo, 270 for big servo and 300 for middle servo
 
 // void writeCustom(Servo servo, int value, int min, int max)
@@ -20,12 +20,12 @@
 
 // void writeSmallServo(Servo servo, int value)
 // {
-//     writeCustom(servo, value, 0, MAX_ANGLE);
+//     writeCustom(servo, value, 0, max_angle);
 // }
 
 // void readSmallServo(Servo servo)
 // {
-//     Serial.println(map(servo.readMicroseconds(), MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, 0, MAX_ANGLE));
+//     Serial.println(map(servo.readMicroseconds(), MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, 0, max_angle));
 // }
 
 // Servo test2;
@@ -35,6 +35,8 @@
 // Servo test6;
 // Servo test7;
 // Servo test8;
+// const int potPin = A6; // Analog input pin A0
+
 // /**
 //  * @brief Setup function
 //  */
@@ -49,13 +51,6 @@
 //     test6.attach(6);
 //     test7.attach(7);
 //     test8.attach(8);
-//     writeSmallServo(test2, 0);
-//     writeSmallServo(test3, 90);
-//     writeSmallServo(test4, 135);
-//     writeSmallServo(test5, 150);
-//     writeSmallServo(test6, 180);
-//     writeSmallServo(test7, 270);
-//     writeSmallServo(test8, 300);
 
 //     delay(1250);
 // }
@@ -65,8 +60,31 @@
 //  */
 // void loop()
 // {
+//     int potValue = analogRead(potPin); // Read the potentiometer value (0-1023)
+//     int new_angle;
+//     if (potValue < 376) {
+//         new_angle = 180;
+//     } else if (potValue < 650) {
+//         new_angle = 270;
+//     } else {
+//         new_angle = 300;
+//     }
+//     if (new_angle != max_angle) {
+//         max_angle = new_angle;
+//         writeSmallServo(test2, 0);
+//         writeSmallServo(test3, 90);
+//         writeSmallServo(test4, 135);
+//         writeSmallServo(test5, 150);
+//         writeSmallServo(test6, 180);
+//         writeSmallServo(test7, 270);
+//         writeSmallServo(test8, 300);
+//     }
+//     delay(500);
+
+//     //* Read the test2 servo angle
 //     // Serial.println(map(test2.readMicroseconds(), MIN_PULSE_WIDTH, MAX_PULSE_WIDTH, 0, 270));
 
+//     //* Write the test2 servo angle to test every angle
 //     // for (int i = 0; i < 300; i++)
 //     // {
 //     //     writeSmallServo(test2, i);
@@ -77,8 +95,5 @@
 //     //     delay(50);
 //     // }
 //     // writeSmallServo(test2, 0);
-//     // delay(1000);
-
-//     // writeSmallServo(test2, 1);
 //     // delay(1000);
 // }
